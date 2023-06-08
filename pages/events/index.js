aca986b9f1cae192549fd79701464e920693549c
pavan <pavanchandupatla24@gmail.com>
2023-06-09 00:45:48 +0530

    module 4 next js max

---
+++ pages/events/index.js
@@ -0,0 +1,24 @@
+import EventList from "@/components/events/event-list";
+import EventsSearch from "@/components/events/event-search";
+import { getAllEvents } from "@/dummy-data"
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
+
+const AllEventsPage =()=>{
+    const events = getAllEvents();
+    const router = useRouter();
+
+    function findEventsHandler(year,month){
+        const fullPath = `/events/${year}/${month}` ;
+        router.push(fullPath)
+    }
+    return(
+        <Fragment>
+            <EventsSearch onSearch={findEventsHandler}/> 
+           <EventList items={events} />
+        </Fragment> 
+    )
+}
+
+export default AllEventsPage
